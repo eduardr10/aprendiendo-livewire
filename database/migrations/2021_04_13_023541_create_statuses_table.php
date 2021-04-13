466be6a55e539a2e0d2a4599f6144b9e4b5f34a8
EduardR10 <EduardR1091@gmail.com>
2021-04-13 14:14:18 -0400

    First Commit

---
+++ database/migrations/2021_04_13_023541_create_statuses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['available', 'out of stock'])->default('out of stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
